bb1f3fd3592f89d9b8f649f0e62ff3dd7420558a
top6543top <111718237+top6543top@users.noreply.github.com>
2023-10-24 22:58:56 +0900

    Update 최대힙2.java

--- 자료구조/11279최대힙/최대힙2.java
+++ 자료구조/11279최대힙/최대힙2.java
@@ -5,22 +5,27 @@ public class Main
     public static int heap[];
     public static int n,index;
     public static void add(int input){
+	//index를 늘리며 배열의 마지막에 들어온 값 저장
         heap[++index]=input;
         
-        int flag=index<<1;
+        int flag=index<<1; // 반복문을 돌리기 위해 미리 비트연산
         while((flag>>=1)>1){
+	    //child node -> parents node:beat calculation 
             if(!swap(flag)) break;
         }
     }
     public static int poll(){
+         // 배열에 아무것도 들어있지 않다면 return 0
         if(index==0) return 0;
         
-        int max=heap[1];
-        heap[1]=heap[index--];
+        int max=heap[1]; // 최대힙의 가장 큰값은 루트노드임으로 저장
+        heap[1]=heap[index--]; // 루트노드를 없애고 그 자리를 매꿔야 함으로 
+	// 가장 작은 노드를 루트노드로 변경
         int i=1;
         while((i<<=1)<=index){
+	    // 만약 같은 레벨에 더 큰값이 있으면 i값을 옮겨 비교
             if(i<index && heap[i]<heap[i+1]) i++;
-            
+            // 부모노드와 자식노드 swap
             if(!swap(i)) break;
         }
         
