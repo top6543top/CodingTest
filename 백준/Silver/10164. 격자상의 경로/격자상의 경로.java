bab9809bf912ebcc99926ef96a2e6187cd5b98c0
UHYUN <111718237+top6543top@users.noreply.github.com>
2026-03-01 16:40:05 +0900

    [Silver II] Title: 격자상의 경로, Time: 104 ms, Memory: 14404 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -3,61 +3,33 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int arr[][]=new int[n][m];
-        int dp[][]=new int[n][m];
-
-        int cnt=1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                arr[i][j]=cnt++;
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            dp[0][i] = 1;
-        }
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = 1;
-        }
-
-        if (k==0){
-            for (int i = 1; i < n; i++) { // 3
-                for (int j = 1; j < m; j++) { // 2
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
-            }
-            System.out.println(dp[n-1][m-1]);
+        if (k == 0) {
+            System.out.println(calculate(n, m));
         } else {
-            int splitx = k % m;
-            int splity = k / m;
-            for (int i = 1; i <= splity; i++) { // 3
-                for (int j = 1; j < splitx; j++) { // 2
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
-            }
+            int ry = (k - 1) / m;
+            int rx = (k - 1) % m;
 
-            for (int i = splity+1; i < n; i++) {
-                dp[i][splitx-1] = 1;
-            }
-            for (int i = splitx; i < m; i++) {
-                dp[splity][i] = 1;
-            }
+            long first = calculate(ry + 1, rx + 1);
+            long second = calculate(n - ry, m - rx);
 
-            for (int i = splity+1; i < n; i++) { // 3
-                for (int j = splitx; j < m; j++) { // 2
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
-            }
-            System.out.println(dp[splity][splitx-1] * dp[n-1][m-1]);
+            System.out.println(first * second);
         }
+    }
 
-
-
+    public static long calculate(int r, int c) {
+        long[][] dp = new long[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (i == 0 || j == 0) dp[i][j] = 1;
+                else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[r - 1][c - 1];
     }
 }
