872767289e93f53fc562650d985e3e27fec84bca
UHYUN <111718237+top6543top@users.noreply.github.com>
2026-03-11 15:01:49 +0900

    [Silver IV] Title: 상근이의 여행, Time: 416 ms, Memory: 56192 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -13,6 +13,7 @@ public class Main {
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
 
+            boolean[] visited = new boolean[n+1];
             ArrayList<Integer>[] graph = new ArrayList[n+1];
             for(int j=0;j<n+1;j++){
                 graph[j]=new ArrayList<>();
@@ -25,10 +26,31 @@ public class Main {
                 graph[a].add(b);
                 graph[b].add(a);
             }
-            sb.append(n-1).append("\n");
+            int result=bfs(graph, visited);
+            sb.append(result-1).append("\n");
         }
 
         System.out.print(sb.toString());
     }
 
+    public static int bfs(ArrayList<Integer>[] graph, boolean[] visited){
+        Queue<Integer> q=new LinkedList<>();
+        q.offer(1);
+        visited[1]=true;
+
+        int result=0;
+        while(!q.isEmpty()){
+            int tmp = q.poll();
+            result++;
+
+            for(int i : graph[tmp]){
+                if(!visited[i]){
+                    //System.out.println("tmp : " + tmp + ", i : "+i);
+                    visited[i]=true;
+                    q.offer(i);
+                }
+            }
+        }
+        return result;
+    }
 }
